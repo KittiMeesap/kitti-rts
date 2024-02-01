3225c40f044704a252d0a55f684ad8f01c0e873f
KittiMeesap <tkt.ms@hotmail.com>
2024-02-01 09:16:34 +0700

    Update Command 0.1

--- Assets/Script/Unit/Unit.cs
+++ Assets/Script/Unit/Unit.cs
@@ -57,6 +57,44 @@ public class Unit : MonoBehaviour
     [SerializeField] private GameObject selectionVisual;
     public GameObject SelectionVisual { get { return selectionVisual; } }
 
+    public void ToggleSelectionVisual(bool flag)
+    {
+        if (selectionVisual != null)
+            selectionVisual.SetActive(flag);
+    }
+
+    public void SetState(UnitState toState)
+    {
+        state = toState;
+
+        if (state == UnitState.Idle)
+        {
+            navAgent.isStopped = true;
+            navAgent.ResetPath();
+        }
+    }
+
+    public void MoveToPosition(Vector3 dest)
+    {
+        if (navAgent != null)
+        {
+            navAgent.SetDestination(dest);
+            navAgent.isStopped = false;
+        }
+
+        SetState(UnitState.Move);
+    }
+
+    private void MoveUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, navAgent.destination);
+
+        if (distance <= 1f)
+            SetState(UnitState.Idle);
+    }
+
+
+
     void Awake()
     {
         navAgent = GetComponent<NavMeshAgent>();
@@ -71,6 +109,12 @@ public class Unit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case UnitState.Move:
+                MoveUpdate();
+                break;
+        }
+
     }
 }
