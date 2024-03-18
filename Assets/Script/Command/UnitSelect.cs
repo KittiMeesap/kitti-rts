2561229474c9f501d8235c8c81e55b0d803bfdb9
KittiMeesap <tkt.ms@hotmail.com>
2024-03-18 09:35:36 +0700

    Add UI for Resource

--- Assets/Script/Command/UnitSelect.cs
+++ Assets/Script/Command/UnitSelect.cs
@@ -20,6 +20,9 @@ public class UnitSelect : MonoBehaviour
     private Building curBuilding; //current selected single building
     public Building CurBuilding { get { return curBuilding; } }
 
+    [SerializeField]
+    private ResourceSource curResource; //current selected resource
+
     public static UnitSelect instance;
 
     void Awake()
@@ -66,7 +69,6 @@ public class UnitSelect : MonoBehaviour
         {
             ShowUnit(curUnit);
         }
-
     }
 
 
@@ -86,6 +88,9 @@ public class UnitSelect : MonoBehaviour
 
                 case "Building":
                     BuildingSelect(hit);
+                    break;
+                case "Resource":
+                    ResourceSelect(hit);
                     break;
             }
         }
@@ -138,4 +143,21 @@ public class UnitSelect : MonoBehaviour
             ShowBuilding(curBuilding);//Show building info
         }
     }
+
+    private void ShowResource()
+    {
+        InfoManager.instance.ShowAllInfo(curResource);//Show resource info in Info Panel
+
+    }
+
+    private void ResourceSelect(RaycastHit hit)
+    {
+        curResource = hit.collider.GetComponent<ResourceSource>();
+        if (curResource == null)
+            return;
+
+        curResource.ToggleSelectionVisual(true);
+        ShowResource();//Show resource info
+    }
+
 }
